c21cff3e64bd2c414569145a7024ffbd115c5e97
BrDanielSousa <danieloliveirasousaa@gmail.com>
2022-07-12 00:23:29 -0300

    first commit

---
+++ br/com/project/campominado/exercao/ExplosaoException.java
@@ -0,0 +1,6 @@
+package br.com.project.campominado.exercao;
+
+public class ExplosaoException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ br/com/project/campominado/modelo/Tabuleiro.java
@@ -0,0 +1,5 @@
+package br.com.project.campominado.modelo;
+
+public class Tabuleiro {
+	
+}
